74cc3163b85fd5a5ac4ba0e246f0341cb45e2f39
Zhivko Draganov <zhivko.draganov@gmail.com>
2022-11-25 15:18:06 +0200

    wip

--- src/api.py
+++ src/api.py
@@ -2,7 +2,9 @@ from aioflask import Flask, request
 
 from nn.models import Models
 from game import AsyncBotBid
+import binary
 import conf
+import sample
 
 app = Flask(__name__)
 
@@ -23,6 +25,55 @@ class PlaceBid:
     self.dealer = place_bid_request.dealer
     self.auction = ['PAD_START'] * DIRECTIONS.index(self.dealer) + place_bid_request.auction
 
+class PlayCard:
+  def __init__(self, play_card_request):
+    self.direction = play_card_request["direction"]
+    self.hand = play_card_request["hand"]
+    self.hand_52 = binary.parse_hand_f(52)(self.hand)
+    self.dummy_hand = play_card_request["dummy_hand"]
+    self.contract = play_card_request["contract"]
+    self.contract_direction = play_card_request["contract_direction"]
+    self.next_player = play_card_request["next_player"]
+    self.dealer = play_card_request["dealer"]
+    self.vuln = VULNERABILITIES[play_card_request.vuln]
+    self.tricks = play_card_request["tricks"]
+
+  def player_index(self):
+    decl_index = DIRECTIONS.index(self.contract_direction)
+    ordered_directions = DIRECTIONS[decl_index:] + DIRECTIONS[:decl_index]
+
+    return ordered_directions.index(self.next_player) - 1
+    
+
+  def rollout_states(self):
+    sample.init_rollout_states(
+      len(self.tricks),
+      player_i, card_players, player_cards_played, shown_out_suits, current_trick, 200, self.padded_auction, card_players[player_i].hand.reshape((-1, 32)), self.vuln, self.models)
+
+# def build_shown_out_suit():
+
+'''
+{
+  "hand": "QJ3.542.KJT7.AQ2",
+  "dummy_hand": "AK2.KQT.AQ52.KJ3",
+  "dealer": "E",
+  "vuln": "None",
+  "auction": [
+    "1C",
+    "PASS",
+    "PASS",
+    "PASS"
+  ],
+  "contract": "2C",
+  "contract_direction: "N",
+  "current_trick_leader": "N",
+  "next_player": "E",
+  "tricks": [
+    ["C2", "C3", "C4", C5"],
+    ["HA"]
+  ]
+}
+'''
 @app.route('/play_card', methods=["POST"])
 async def play_card():
   pass
@@ -47,8 +98,6 @@ async def place_bid():
     bot = AsyncBotBid(
       req.vuln,
       req.hand,
-      [False, False],
-      "QJ3.542.KJT7.AQ2",
       MODELS
     )
 
@@ -64,4 +113,4 @@ async def healthz():
   return {'status': 'ok'}
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8081, use_reloader=True)
+    app.run(host='0.0.0.0', port=8081, debug=True, use_reloader=True)

b983e746e84bd158a11e8e7dc0d33174d742fd02
lucbellicaud <68165378+lucbellicaud@users.noreply.github.com>
2023-01-31 14:57:26 +0100

    Make ben quicker to follow/discard

--- src/transform_play_card.py
+++ src/transform_play_card.py
@@ -15,8 +15,19 @@ import sample
 
 from game import AsyncCardPlayer
 
+def get_play_status(hand : PlayerHand, current_trick: List[Card_]):
+    if current_trick == [] or len(current_trick)==4:
+        return "Lead"
+    match len(hand.suits[current_trick[0].suit]):
+        case 0:
+            return "Discard"
+        case 1:
+            return "Forced"
+        case _:
+            return "Follow"
 
 async def get_ben_card_play_answer(hand_str, dummy_hand_str, dealer_str, vuln_str, auction, contract, declarer_str, next_player_str, tricks_str, MODELS) -> str:
+    n_samples = 200
     padded_auction = ["PAD_START"] * \
         Direction.from_str(dealer_str).value + auction
 
@@ -91,8 +102,13 @@ async def get_ben_card_play_answer(hand_str, dummy_hand_str, dealer_str, vuln_st
 
             card_i += 1
             if card_i >= len(play):
+                play_status = get_play_status(hands_for_diag[next_player],[Card_.from_str(card) for card in tricks_str[-1]])
+                if play_status=="Follow" :
+                    n_samples=50
+                if play_status=="Discard" :
+                    n_samples = 50
                 rollout_states = sample.init_rollout_states(trick_i, player_i, card_players, player_cards_played, shown_out_suits,
-                                                            current_trick, 200, padded_auction, card_players[player_i].hand.reshape((-1, 32)), vuls, MODELS)
+                                                            current_trick, n_samples, padded_auction, card_players[player_i].hand.reshape((-1, 32)), vuls, MODELS)
                 resp = await card_players[player_i].async_play_card(trick_i, leader_i, current_trick52, rollout_states)
                 return play_real_card(random_diag.hands[next_player], list(resp.to_dict().values())[0], trump=BiddingSuit.from_str(contract[1]), play_record=play_record, player_direction=next_player, declarer=declarer).__str__()
 

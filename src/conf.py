3efb117033caa0a802b74478734774800a036cfb
Zhivko Draganov <zhivko.draganov@gmail.com>
2023-02-09 14:22:03 +0200

    fix: Revert the conf parser

--- src/conf.py
+++ src/conf.py
@@ -1,9 +1,8 @@
 from configparser import ConfigParser
-import os
 
 
 def load(fnm: str) -> ConfigParser:
     conf = ConfigParser()
-    with open(os.getcwd()+"\\"+"default.conf") as f:
+    with open(fnm) as f:
         conf.read_file(f)
     return conf

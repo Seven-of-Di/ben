a1e875d2347c52f3c139b40ac036eb1ebbab0cd5
Zhivko Draganov <zhivko.draganov@gmail.com>
2023-03-06 19:20:20 +0200

    feat: Reorganize logging for the health checker

--- src/health_checker.py
+++ src/health_checker.py
@@ -13,17 +13,15 @@ class HealthChecker:
     def healthy(self) -> bool:
         for cpu_per_core in self.cpu_usage:
             if cpu_per_core > 95:
+                self.logger.warning(self.cpu_usage)
                 return False
 
         return True
 
     def start(self):
-        self.logger.warning("Started HealthChecker")
-
         x = threading.Thread(target=self.start_metrics_loop)
         x.start()
 
     def start_metrics_loop(self):
         while True:
             self.cpu_usage = psutil.cpu_percent(interval=15, percpu=True)
-            self.logger.warning(self.cpu_usage)
